ec761e0c9f46975c9a3ee86eb0714b6d6bd5c364
teovoinea <voinea.teodor@gmail.com>
2018-03-10 17:50:30 -0500

    Start adding CI and cleanup

--- install.bat
+++ install.bat
@@ -1,1 +1,7 @@
-REM TODO(teo): implement this for windows
+cargo install spot-server
+git clone https://github.com/teovoinea/spot-client
+cd spot-client
+cargo web deploy --release --target=wasm32-unknown-unknown
+cd ..
+xcopy /y .\spot-client\target\deploy\* .\static\
+cargo build --release
--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 cargo install spot-server
 git clone https://github.com/teovoinea/spot-client
 cd spot-client
-cargo web deploy --release --target=wasm32-unknown-unknown
+./install_cargo_web.sh
+cargo-web deploy --release --target=wasm32-unknown-unknown
 cd ..
-cp spot-client/target/deploy/* static
+cp spot-client/target/deploy/* static/
+cargo build --release
